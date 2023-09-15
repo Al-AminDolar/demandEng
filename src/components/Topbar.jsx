9f6a33187cd85a934fca043a733f59d1ffa3388c
alamin <alamin.dolar1@gmail.com>
2023-09-15 22:42:02 +0600

    react icons package added

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,11 +1,11 @@
 export default function topbar() {
   return (
-    <div className=" text-white font-mono flex h-8 items-center bg-slate-700  ">
-      <div className="flex justify-between container mx-auto">
+    <div className=" text-white font-mono flex h-12 items-center bg-[#181818]  ">
+      <div className="container mx-auto flex justify-between ">
         <h3>Best industrial service</h3>
         <div className="flex gap-3">
           <h3>Facebook</h3>
-          <h3>WhatsUp: +880-1974818291</h3>
+          <h3>WhatsUp: +8801974818291</h3>
         </div>
       </div>
     </div>

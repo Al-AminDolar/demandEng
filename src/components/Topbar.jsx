f1ce42cbdb0e68e4ce7f7a42228bbc9f19da7a44
alamin <alamin.dolar1@gmail.com>
2023-09-16 00:12:18 +0600

    top bar design done

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,11 +1,39 @@
+import { BiPhoneCall, BiTimeFive } from "react-icons/bi";
+import { MdOutlineMarkEmailRead } from "react-icons/md";
+
 export default function topbar() {
   return (
-    <div className=" text-white font-mono flex h-12 items-center bg-[#181818]  ">
-      <div className="container mx-auto flex justify-between ">
-        <h3>Best industrial service</h3>
-        <div className="flex gap-3">
-          <h3>Facebook</h3>
-          <h3>WhatsUp: +8801974818291</h3>
+    <div className=" text-white font-mono bg-[#181818] py-3 ">
+      <div className="container mx-auto grid grid-cols-3 justify-around items-center ">
+        <div className="col-span-1">
+          <h3 className="text-xl ">
+            Best <span className="text-orange-600">industrial</span> service
+          </h3>
+        </div>
+        <div className="col-span-2 flex gap-9 justify-center">
+          <div className="flex items-center gap-4 text-white ">
+            <BiPhoneCall className="h-9 w-9 text-white" />
+            <div className="leading-5">
+              <span className="text-orange-600 text-sm">
+                Call us 9.00am-6.00pm
+              </span>{" "}
+              <h3>01934559622</h3>
+            </div>
+          </div>
+          <div className="flex items-center gap-4 text-white">
+            <BiTimeFive className="h-9 w-9 text-white" />
+            <div className="leading-5">
+              <span className="text-orange-600 text-sm">Monday to Friday</span>{" "}
+              <h3>9:00am - 6:00pm</h3>
+            </div>
+          </div>
+          <div className="flex items-center gap-4 text-white">
+            <MdOutlineMarkEmailRead className="h-9 w-9 text-white" />
+            <div className="leading-5">
+              <span className="text-orange-600 text-sm">E-mail us</span>{" "}
+              <h1>support@demandeng.com</h1>
+            </div>
+          </div>
         </div>
       </div>
     </div>

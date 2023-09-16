2c27ed3df9851b014febcbca6df3d307983b0eba
alamin <alamin.dolar1@gmail.com>
2023-09-16 12:23:20 +0600

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
+import "../styles/global.css";
 export default function Navbar() {
   return (
     <div>
-      <nav className="container mx-auto">
-        <h1 className="text-2xl">Logo</h1>
-        <div className="navigation">
-          <a href="">Home</a>
-          <a href="">Services</a>
-          <a href="">Solutions</a>
-          <a href="">About us</a>
-          <a href="">Contact us</a>
+      <nav className="container mx-auto flex items-center justify-between">
+        <div className="Logo leading-3">
+          <h1 className="text-3xl font-semibold tracking-widest">DEMAND</h1>
+          <span className="text-orange-600  font-bold ">Engineering</span>
+        </div>
+        <div className="navigation flex gap-4 text-xl font-semibold">
+          <a href="#">Services</a>
+          <a href="#">Solutions</a>
+          <a href="#">About us</a>
+          <a href="#">Contact us</a>
         </div>
       </nav>
     </div>

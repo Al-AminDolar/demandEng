70f5f5ea6bfc9a9c3cbeb499e62c5637ea79f58d
alamin <alamin.dolar1@gmail.com>
2023-09-17 21:09:26 +0600

    banner done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,3 +1,4 @@
+import { BsArrowRight } from "react-icons/bs";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
@@ -27,7 +28,21 @@ const Banner = () => {
           <img src="/public/image/banner-1-3.jpg" alt="" />
         </div>
       </Slider>
-      <div className="carousel-text">Your single text here</div>
+      <div className="carousel-text  text-center  flex flex-col ">
+        <div className="font-semibold text-5xl">
+          <h1>We will provide the</h1>
+          <h1>
+            best <span className="text-[#3ee438]">Industrial</span> support
+          </h1>
+        </div>
+
+        <button className="bg-gradient-to-r from-red-500 to-orange-600 px-7 py-3 flex items-center justify-center gap-3 my-7 text-lg font-semibold ">
+          Learn More{" "}
+          <span className="text-xl ">
+            <BsArrowRight />
+          </span>
+        </button>
+      </div>
     </div>
   );
 };

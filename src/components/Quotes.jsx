4ecff03c4d5939311b6d6b37ccad15115a5f4bdc
alamin <alamin.dolar1@gmail.com>
2023-09-23 21:32:44 +0600

    antd collapse section added

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,9 +1,38 @@
+import { Collapse } from "antd";
 import { BiSolidRightArrowCircle } from "react-icons/bi";
 const Quotes = () => {
+  const text = (
+    <p
+      style={{
+        paddingLeft: 24,
+      }}
+    >
+      A dog is a type of domesticated animal. Known for its loyalty and
+      faithfulness, it can be found as a welcome guest in many households across
+      the world.
+    </p>
+  );
+  const items = [
+    {
+      key: "1",
+      label: "This is panel header 1",
+      children: text,
+    },
+    {
+      key: "2",
+      label: "This is panel header 2",
+      children: text,
+    },
+    {
+      key: "3",
+      label: "This is panel header 3",
+      children: text,
+    },
+  ];
   return (
     <div className="container mx-auto">
       <div className="grid grid-cols-5 gap-10">
-        <div className="col-span-3 bg-blue-500 p-3 ">
+        <div className="col-span-3  p-8 ">
           <h1 className="text-4xl font-semibold py-4">
             Learn more from our Faq__
           </h1>
@@ -11,8 +40,17 @@ const Quotes = () => {
             You will find yourself working in a true partnership that results in
             an incredible experience, and an end product that is the best.
           </p>
+          <div className="my-8">
+            <Collapse
+              size="large"
+              className="bg-gray-100"
+              items={items}
+              bordered={false}
+              defaultActiveKey={["1"]}
+            />
+          </div>
         </div>
-        <div className="col-span-2 bg-orange-500">
+        <div className="col-span-2 ">
           <div className="bg-gray-900 p-8">
             <h1 className="text-2xl font-semibold text-white  pb-3">
               Request a <span>Quote</span> __

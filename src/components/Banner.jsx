1334c3de9ec6b1a01a584c9402f11d02d8d969de
alamin <alamin.dolar1@gmail.com>
2023-09-25 01:50:30 +0600

    caurosel text responsive for lg

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -20,25 +20,25 @@ const Banner = () => {
       text: (
         <div className="carousel-text ">
           <div className="font-semibold">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-4xl 2xl:text-5xl">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
               Demand Engineering
             </h1>
-            <h4 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-xl 2xl:text-2xl mb-4">
+            <h4 className="text-xl sm:text-2xl md:text-3xl lg:text-xl xl:text-xl 2xl:text-2xl mb-4">
               Your Demand Our Service
             </h4>
 
-            <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-2xl 2xl:text-3xl">
+            <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-2xl xl:text-2xl 2xl:text-3xl">
               We will provide the
             </h1>
-            <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-xl 2xl:text-xl">
+            <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-xl xl:text-xl 2xl:text-xl">
               best <span className="text-[#3ee438]">Industrial</span> machinery
               support
             </h1>
           </div>
 
-          <button className="bg-gradient-to-r from-red-500 to-orange-600  sm:px-6 md:px-8 lg:px-10 xl:px-2 2xl:px-3  sm:py-3 md:py-4 lg:py-5 xl:py-1 2xl:py-1 flex items-center justify-center gap-3 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-lg 2xl:text-xl font-semibold mt-3">
+          <button className="bg-gradient-to-r from-red-500 to-orange-600  sm:px-6 md:px-8 lg:px-2 xl:px-2 2xl:px-3  sm:py-3 md:py-4 lg:py-1 xl:py-1 2xl:py-1 flex items-center justify-center gap-3 text-base sm:text-lg md:text-xl lg:text-sm xl:text-lg 2xl:text-xl font-semibold mt-3">
             Learn More{" "}
-            <span className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-lg 2xl:text-xl">
+            <span className="text-lg sm:text-xl md:text-2xl lg:text-sm xl:text-lg 2xl:text-xl">
               <BsArrowRight />
             </span>
           </button>

6ba9dcf29f864a672780a742f2f70ae727f0f595
alamin <alamin.dolar1@gmail.com>
2023-09-28 16:40:44 +0600

    responsive footer ready

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,76 +1,94 @@
 import {
   FaLinkedin,
-  FaLocationDot,
-  FaPhoneVolume,
   FaSquareFacebook,
   FaSquareWhatsapp,
 } from "react-icons/fa6";
-import { MdEmail } from "react-icons/md";
+import { NavLink } from "react-router-dom";
 import "../styles/global.css";
 
 const Footer = () => {
   return (
-    <div className=" bg-gray-900 text-white  container mx-auto">
-      <div className="grid grid-cols-1 md:grid-cols-4 ">
-        <div className="About p-3">
-          <h1>About Us</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error,
-            nulla! Modi, facere rerum veniam quam explicabo accusamus dolorum
-            quas ratione?
-          </p>
-          <span className="flex">
-            <FaSquareFacebook /> <FaSquareWhatsapp /> <FaLinkedin />
-          </span>
-        </div>
-        <div className="OurServices p-3">
-          <h2>Our Services</h2>
-          <ul>
-            <li>AIR COMPRESSOR MANUFACTURE</li>
-            <li>MULTI-BRAND AIR COMPRESSOR SERVICE</li>
-            <li>COMPRESSOR SPARES MANUFACTURE</li>
-            <li>AIR COMPRESSOR SALES</li>
-          </ul>
-        </div>
-        <div className="getintouch p-3">
-          <h2>Get in Touch</h2>
-          <h6>
-            {" "}
-            <span>
-              <FaLocationDot />
+    <div className="bg-[#262626] text-white ">
+      <div className="flex justify-center container mx-auto ">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-5 pb-20 pt-10">
+          <div className="About p-3">
+            <h1 className="text-xl font-medium pb-4">About Us</h1>
+            <p className="text-base leading-relaxed">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error,
+              nulla! Modi, facere rerum veniam quam explicabo accusamus dolorum
+              quas ratione?
+            </p>
+            <span className="flex text-3xl pt-2 gap-2">
+              <NavLink>
+                <FaSquareFacebook />
+              </NavLink>
+              <NavLink>
+                <FaSquareWhatsapp />
+              </NavLink>
+              <NavLink>
+                <FaLinkedin />
+              </NavLink>
             </span>
-            <span>Address:</span> House #64, Road #07,Shekertek,Mohammadpur,
-            Dhaka - 1207{" "}
-          </h6>
-          <h6>
-            <span>
-              <FaPhoneVolume />
-            </span>{" "}
-            +880 01325-435437
-          </h6>
-          <h6>
-            <span>
-              <MdEmail />
-            </span>{" "}
-            +880 01325-435437
-          </h6>
-        </div>
-        <div className="QuickLinks p-3">
-          <h2>Quick Links</h2>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contacts</li>
-            <li>Shop</li>
-          </ul>
+          </div>
+          <div className="OurServices p-3 ">
+            <h2 className="text-xl font-medium pb-4">Our Services</h2>
+            <ul className="text-base leading-relaxed font-semibold">
+              <li>
+                <NavLink> - Air Compressor Manufacture</NavLink>{" "}
+              </li>
+              <li>
+                <NavLink> - Multi-brand Air Compressor Service</NavLink>
+              </li>
+              <li>
+                <NavLink> - Compressor Spares Manufacture</NavLink>
+              </li>
+              <li>
+                <NavLink> - Air Compressor Sales</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className=" getintouch p-3 ">
+            <h2 className="text-xl font-medium pb-4">Get in Touch</h2>
+            <p className="text-base leading-relaxed">
+              <span className="font-bold">Address : </span>
+              <span>
+                {" "}
+                House #64,Road #07, Shekertek, Mohammadpur,Dhaka-1207
+              </span>
+            </p>
+            <p className="text-sm leading-relaxed">
+              <span className="font-bold">Mobile : </span>
+              <span> +880 01325-435437</span>
+            </p>
+            <p className="text-sm leading-relaxed ">
+              <span className="font-bold">Email : </span>
+              <span>munna@demandeng.com</span>
+            </p>
+          </div>
+          <div className="QuickLinks p-3">
+            <h2 className="text-xl font-medium pb-4">Quick Links</h2>
+            <ul className="text-base leading-relaxed font-semibold">
+              <li>
+                <NavLink>Home</NavLink>
+              </li>
+              <li>
+                <NavLink>About</NavLink>
+              </li>
+              <li>
+                <NavLink>Contacts</NavLink>
+              </li>
+              <li>
+                <NavLink>Shop</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-
       {/* footer end Part */}
       <div className="border-t-[1px] bg-slate-200 flex justify-center p-4">
-        <h1 className="text-slate-600">
-          © Copyright Demand Engineering 2021. All rights reserved.
-        </h1>
+        <p className="text-slate-600">
+          © Copyright Demand Engineering 2021 . All rights reserved.
+        </p>
       </div>
     </div>
   );

8ee51dc2e2b2910ce678244ff3097a4d0066775b
alamin <alamin.dolar1@gmail.com>
2023-10-01 01:23:42 +0600

    idea section modify

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,12 @@ import "../styles/global.css";
 const Card = () => {
   return (
     <div className="bg-slate-100 pt-10 pb-20 ">
-      <h1 className="text-5xl pt-3  font-medium text-center">
+      <h1 className="text-4xl pt-3  font-medium text-center">
         Our <span className="text-purple-700">Services</span>
       </h1>
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 p-7">
         <div className="card text-center">
-          <img className="card-image" src={compressor} alt="Air Compressor" />
+          <img className="card-image " src={compressor} alt="Air Compressor" />
           <h1 className="text-2xl font-semibold py-3">Air Compressor</h1>
 
           <div className="flex justify-center">

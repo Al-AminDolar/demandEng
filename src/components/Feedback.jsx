cfd5e3ae3c1155b98ca20f688120242fed01591b
alamin <alamin.dolar1@gmail.com>
2023-10-01 22:10:26 +0600

    almost done home page

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -3,7 +3,7 @@ import image1 from "../images/feedback1.jpg";
 const Feedback = () => {
   return (
     <div className="bg-[url('../images/map-bg.png')] bg-cover bg-center  bg-[#222222] ">
-      <h1 className="text-white text-3xl font-semibold text-center pt-6">
+      <h1 className="text-white text-xl font-semibold text-center pt-6">
         What People Are Saying
       </h1>
       <div className="container mx-auto  items-center ">
@@ -11,10 +11,9 @@ const Feedback = () => {
           {/* Feedback One */}
           <div className="border p-8 rounded-md m-8">
             <p className="text-justify font-medium text-white">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
-              eaque temporibus atque molestias cupiditate a aspernatur earum
-              nemo maxime laboriosam commodi eveniet fugiat, eos suscipit? Dolor
-              quasi id perspiciatis quo.
+              They are very awesome ,their team management system very well and
+              strong. They have enougph experienced in this field. I am very
+              happy to sharing that you should contact them for your demand
             </p>
             <div className="flex items-center gap-4">
               <img className="rounded-full my-3 border-2" src={image1} alt="" />

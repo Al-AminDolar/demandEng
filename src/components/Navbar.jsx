d9dfd0edadd4bd2a2dd2762ddc71b8664f706812
alamin <alamin.dolar1@gmail.com>
2023-10-01 21:08:35 +0600

    fully responsive navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,78 +1,51 @@
 import { useState } from "react";
 import log from "../images/logo.png";
-
 const Navbar = () => {
-  const [menuName, setMenuName] = useState("menu");
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const handleMenuToggle = () => {
-    setMenuName((prevName) => (prevName === "menu" ? "close" : "menu"));
-    setMenuOpen((prevMenuOpen) => !prevMenuOpen);
-  };
-
+  let Links = [
+    { name: "HOME", link: "/" },
+    { name: "SERVICE", link: "/" },
+    { name: "ABOUT", link: "/" },
+    { name: "BLOG'S", link: "/" },
+    { name: "CONTACT", link: "/" },
+  ];
+  let [open, setOpen] = useState(false);
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 p-3 text-xl font-medium bg-white shadow-md md:flex md:items-center md:justify-between ">
-      <div className="flex justify-between items-center w-full max-w-screen-xl mx-auto">
-        <span className="text-2xl font-[Poppins] cursor-pointer">
-          <img className="h-10 inline" src={log} alt="Tailwind Logo" />
-        </span>
-
-        <div>
-          <span
-            className="text-xl pl-3 cursor-pointer md:hidden block"
-            onClick={handleMenuToggle}
-          >
-            <ion-icon name={menuName}></ion-icon>
+    <div className="shadow-md w-full fixed z-10 top-0 left-0">
+      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
+        <div
+          className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+    text-gray-800"
+        >
+          <span className="text-2xl font-[Poppins] cursor-pointer">
+            <img className="h-10 inline" src={log} alt="Tailwind Logo" />
           </span>
-          <ul
-            className={`md:flex md:items-center md:flex-grow ${
-              menuOpen ? " block" : "hidden "
-            }`}
-          >
-            <li className="mx-4 my-2 md:my-0">
-              <a
-                href="#"
-                className="text-base hover:text-orange-500 duration-500"
-              >
-                Home
-              </a>
-            </li>
-            <li className="mx-4 my-2 md:my-0">
-              <a
-                href="#"
-                className="text-base hover:text-orange-500 duration-500"
-              >
-                Service
-              </a>
-            </li>
-            <li className="mx-4 my-2 md:my-0">
-              <a
-                href="#"
-                className="text-base hover:text-orange-500 duration-500"
-              >
-                Shop
-              </a>
-            </li>
-            <li className="mx-4 my-2 md:my-0">
-              <a
-                href="#"
-                className="text-base hover:text-orange-500 duration-500"
-              >
-                About
-              </a>
-            </li>
-            <li className="mx-4 my-2 md:my-0">
-              <a
-                href="#"
-                className="text-base hover:text-orange-500 duration-500"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
         </div>
+
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+        >
+          <ion-icon name={open ? "close" : "menu"}></ion-icon>
+        </div>
+
+        <ul
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-20 " : "top-[-490px]"
+          }`}
+        >
+          {Links.map((link) => (
+            <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
+              <a
+                href={link.link}
+                className="text-gray-800 hover:text-gray-400 duration-500"
+              >
+                {link.name}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
-    </nav>
+    </div>
   );
 };
 

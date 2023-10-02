bffaf89cd725f725157ac216580e91c237b3ad75
alamin <alamin.dolar1@gmail.com>
2023-10-02 19:26:47 +0600

    card added in shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,16 @@
+import ShopCard from "./ShopCard";
+
 const Shop = () => {
   return (
-    <div className="container mx-auto">
-      <h1>Welome to Our Shop</h1>
+    <div className="container  mx-auto mt-28">
+      <h1 className="text-center text-2xl mb-10 font-serif font-semibold">
+        Welome to Our Shop
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+        {[1, 2, 3].map((data) => {
+          return <ShopCard key={data} />;
+        })}
+      </div>
     </div>
   );
 };

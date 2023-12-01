a8c635ac979d282c58cabb05d7ff7c13363e30a6
alamin <alamin.dolar1@gmail.com>
2023-12-02 02:25:36 +0600

    service page 40% done

--- src/components/Main/Services/Services.jsx
+++ src/components/Main/Services/Services.jsx
@@ -1,3 +1,6 @@
+import { FaCheck } from "react-icons/fa";
+import Callcenter from "../../../images/CustomerService.png";
+import Rotary from "../../../images/compressor.png";
 import Service from "./Service";
 
 const Services = () => {
@@ -24,10 +27,73 @@ const Services = () => {
     ],
   ];
   return (
-    <div className=" mt-24 mb-20 max-h-min  ">
-      <h1 className="text-3xl text-purple-800 font-semibold text-center ">
+    <div className=" mt-32 mb-20 max-h-min container mx-auto ">
+      <h1 className="text-3xl text-purple-800 font-semibold font-serif ">
         Our Services
       </h1>{" "}
+      <p className="w-1/2 text-justify">
+        Demand Engineering Solution is capable of providing industry leading to
+        our customers through its professional team of engineers. All of our
+        engineers are certified by the professional engineers institutions.
+      </p>
+      <section className="mt-10">
+        <div className="flex justify-center items-center gap-10">
+          <div className="space-y-7">
+            <div className="flex items-center gap-3">
+              <span className="text-green-500 text-2xl">
+                <FaCheck />
+              </span>
+              <p className=" font-semibold">24/7 call center service </p>
+            </div>
+            <div className="flex items-center gap-3">
+              <span className="text-green-500 text-2xl">
+                <FaCheck />
+              </span>
+              <p className=" font-semibold">Provide technician support </p>
+            </div>
+            <div className="flex items-center gap-3">
+              <span className="text-green-500 text-2xl">
+                <FaCheck />
+              </span>
+              <p className=" font-semibold">Provide engineering support </p>
+            </div>
+          </div>
+          <div>
+            <img className="rounded-md" src={Callcenter} alt="" />
+          </div>
+        </div>
+      </section>
+      <section className="mt-12">
+        <div className="flex items-center justify-center gap-10">
+          <div className="w-[300px]">
+            <img src={Rotary} alt="" />
+          </div>
+          <div>
+            <h1>Rotary Scew Compressor</h1>
+            <div className="flex items-center gap-3">
+              <span className="text-green-500 text-2xl">
+                <FaCheck />
+              </span>
+              <p className=" font-semibold">
+                Oil Floated Rotery Screw: Capacity range: 30 CFM to 2000 CFM{" "}
+              </p>
+            </div>
+            <div className="flex items-center gap-3">
+              <span className="text-green-500 text-2xl">
+                <FaCheck />
+              </span>
+              <p className=" font-semibold">
+                Oil Free Rotery Screw: Capacity range: 170 CFM to 2500 CFM{" "}
+              </p>
+            </div>
+            <p>1. IE4 Motor</p>
+            <p>2. High performance Airend</p>
+            <p>3. Low maintenance cost</p>
+            <p>4. Easy operation</p>
+            <p>4. Vibration free & low noise</p>
+          </div>
+        </div>
+      </section>
       <div className="flex  justify-center mt-10">
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3 mb-10 ">
           {servicelist[0].map((data, i) => {

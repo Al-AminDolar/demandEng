7cf920c77bbc7b9f17105612dbe887a6ad2a670c
alamin <alamin.dolar1@gmail.com>
2023-12-24 21:12:27 +0600

    filter page done

--- src/pages/ScrewCompressorFilter.jsx
+++ src/pages/ScrewCompressorFilter.jsx
@@ -1,3 +1,4 @@
+import { FaTools } from "react-icons/fa";
 import { TiSpanner } from "react-icons/ti";
 const ScrewCompressorFilter = () => {
   return (
@@ -158,13 +159,13 @@ const ScrewCompressorFilter = () => {
         </div>
       </section>
 
-      <section className="p-10">
+      <section className="">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-0 items-center justify-center">
           <div className="flex justify-center items-center">
             <img
               src="https://i.ibb.co/f4hYksb/FINAL-FULL-20.png"
               alt=""
-              className="h-[180px] md:h-[300px] "
+              className="h-[180px] md:h-[250px] "
             />
           </div>
           <div className="flex justify-center items-center">
@@ -174,12 +175,151 @@ const ScrewCompressorFilter = () => {
                 <div className="h-[2px] w-[100px] bg-orange-500 "></div>
               </h4>
               <p className="text-justify text-sm md:text-base mt-2 md:w-[70%]">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-                eaque nihil porro nesciunt, officiis incidunt minima similique
-                eligendi explicabo, ut qui. Corporis, autem? Ut aliquam
-                excepturi deserunt eaque sequi sit.
+                It is a filter within an air compressor system that separates
+                oil from the compressed air to proctect systems components and
+                your equipment at the end of the line. Lubricated rotary air
+                compressors mix oil with the intake air to lubricate the
+                compressor.
               </p>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-[#f1f5f9] p-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-0 container mx-auto">
+          <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center">
+            <div className=" flex justify-center ">
+              <img
+                className="h-[150px] md:h-[200px]"
+                src="https://i.ibb.co/zhKGRwx/FINAL-FULL-19-removebg-preview.png"
+                alt=""
+              />
+            </div>
+
+            <div className="">
+              <h4 className="font-semibold">
+                Kit Items:
+                <div className="h-[2px] w-[70px] bg-orange-500 "></div>
+              </h4>
+              <div className="grid grid-cols-2">
+                <div className="flex items-center gap-[4px] text-sm md:text-base mt-1">
+                  <div className="text-orange-500 text-xs">
+                    <FaTools />
+                  </div>
+                  <div>
+                    <p>TCB</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px] text-sm md:text-base">
+                  <div className="text-orange-500 text-xs">
+                    <FaTools />
+                  </div>
+                  <div>
+                    <p>Blow Down</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px] text-sm md:text-base">
+                  <div className="text-orange-500 text-xs">
+                    <FaTools />
+                  </div>
+                  <div>
+                    <p>Load Unload</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px] text-sm md:text-base">
+                  <div className="text-orange-500 text-xs">
+                    <FaTools />
+                  </div>
+                  <div>
+                    <p>Intake valve</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px] text-sm md:text-base">
+                  <div className="text-orange-500 text-xs">
+                    <FaTools />
+                  </div>
+                  <div>
+                    <p>Scavenge Orifice</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px] text-sm md:text-base">
+                  <div className="text-orange-500 text-xs">
+                    <FaTools />
+                  </div>
+                  <div>
+                    <p>MPV</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px] text-sm md:text-base">
+                  <div className="text-orange-500 text-xs">
+                    <FaTools />
+                  </div>
+                  <div>
+                    <p>Auto Drain</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center">
+            <div className="flex justify-center items-center">
+              <img
+                className="h-[150px] md:h-[200px] rounded-xl "
+                src="https://i.ibb.co/9qSfhHF/FINAL-FULL-18.png"
+                alt=""
+              />
+            </div>
+
+            <div>
+              <h4 className="font-semibold">
+                Lubracating Oil:
+                <div className="h-[2px] w-[70px] bg-orange-500 "></div>
+              </h4>
+              <p className=" text-sm md:text-base mt-2">
+                Nearly all compressors require lubricating,cooling,sealing and
+                lubricate internal components.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="container mx-auto">
+        <div className="  p-10  grid grid-cols-1 divide-x  md:grid-cols-3 devide-x  gap-6 divide-gray-300  ">
+          <div className="p-5">
+            <h4 className="font-semibold">
+              Product Origin:
+              <div className="h-[2px] w-[70px] bg-orange-500 "></div>
+            </h4>
+            <p className=" text-sm md:text-base mt-2 text-justify">
+              Our all product are inported from top parts manufacturers of EU,
+              India, China and Singapore. Most of parts brand are origin which
+              we collect China and Singapore distribution. The quality and
+              lonngevity of these spares are equal.
+            </p>
+          </div>
+          <div className="p-5">
+            <h4 className="font-semibold">
+              Product Delivery:
+              <div className="h-[2px] w-[70px] bg-orange-500 "></div>
+            </h4>
+            <p className=" text-sm md:text-base mt-2 text-justify">
+              Usually we maintain all demandable items and supply within 1-3
+              days form out stock. If we have not available any special non
+              useable parts we are capable to supply it 5-6 weeks.
+            </p>
+          </div>
+          <div className="p-5">
+            <h4 className="font-semibold">
+              Product Warrenty:
+              <div className="h-[2px] w-[70px] bg-orange-500 "></div>
+            </h4>
+            <p className=" text-sm md:text-base mt-2 ">
+              Our product will be guaranteed for 2000 hours under normal working
+              operation condition.
+            </p>
           </div>
         </div>
       </section>

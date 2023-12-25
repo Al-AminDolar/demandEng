319045fb6aee49af227074f38522fb9ad168ec92
alamin <alamin.dolar1@gmail.com>
2023-12-25 17:31:09 +0600

    all work done

--- src/pages/ElectricalItem.jsx
+++ src/pages/ElectricalItem.jsx
@@ -1,5 +1,488 @@
+import { TiSpanner } from "react-icons/ti";
 const ElectricalItem = () => {
-  return <div></div>;
+  return (
+    <div className="mt-[75px]  ">
+      <div className="bg-[#f9fafb] p-5 px-14 border-b shadow-sm">
+        {" "}
+        <h1 className="text-xl text-slate-600">Electrical Section</h1>
+      </div>
+      <section className=" p-10 container mx-auto ">
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-center  gap-6 md:gap-0">
+          <div className="order-2 md:order-1 md:p-20">
+            <div className="space-y-2">
+              <h4 className="text-base font-medium">
+                LV & MV Circuit Breakers Contractors & Overload Relays:
+                <div className="h-[2px] w-[150px] md:w-[400px] bg-orange-500 "></div>
+              </h4>
+
+              <div className="grid  text-sm md:text-base ">
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>All types of Circuit Breaker</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>All type of Magnetic Contactor</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>All type of Overload Relays</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Thermal Overload Relays</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Band: ABB Schneider</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="order-1 md:order-2 flex justify-center items-center">
+            <img
+              className="w-[250px] rounded-2xl border border-sky-500  shadow-sm shadow-sky-500 md:w-[400px] "
+              src="https://i.ibb.co/GJ5CQ5F/FINAL-FULL-31.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className=" p-10   bg-[#f1f5f9]">
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-center  gap-6 md:gap-0 container mx-auto ">
+          <div className="order-2 md:order-2 md:p-20">
+            <div className="space-y-2">
+              <h4 className="text-base font-medium">
+                Distribution Transformer:
+                <div className="h-[2px] w-[140px]  bg-orange-500 "></div>
+              </h4>
+
+              <div className="grid  text-sm md:text-base ">
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Three Phase Distribution Transformer</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>
+                      Energy efficient oil immersed distribution transformer
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Environment-friendly cast resin dry type Transformer</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Capacity: 50 KVA to 3000 KVA</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Voltage class: 11/.440 KV</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Indoor and Outdoor installation facility</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>In accordance with IEC 60076 standard</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="order-1 md:order-1 flex justify-center items-center">
+            <img
+              className="w-[200px] rounded-2xl border border-sky-500  shadow-sm shadow-sky-500 md:w-[300px] "
+              src="https://i.ibb.co/8Ys4hgL/FINAL-FULL-29.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+
+      <section className=" p-10  ">
+        <div className="grid grid-cols-1 md:grid-cols-2 justify-center  gap-6 md:gap-0 container mx-auto ">
+          <div className="order-2 md:order-1 md:p-20">
+            <div className="space-y-2">
+              <h4 className="text-base font-medium">
+                Switchgear:
+                <div className="h-[2px] w-[140px]  bg-orange-500 "></div>
+              </h4>
+
+              <div className="grid  text-sm md:text-base ">
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Low and Medium Voltage metal enclosed switchgear</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>In accordance with relevant IEC standard</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Vacuum circuit breaker 630A to 1250A</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Load Break Switch 630A 1250A</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>LT Panel up to 6300A</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Power factor Improvement (PFI) panel</p>
+                  </div>
+                </div>
+                <div className="flex items-center gap-[4px]">
+                  <div className="text-orange-500 ">
+                    <TiSpanner />
+                  </div>
+                  <div>
+                    <p>Compact & customized design</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="order-1 md:order-2 flex justify-center items-center">
+            <img
+              className="w-[200px] rounded-2xl border border-sky-500  shadow-sm shadow-sky-500 md:w-[300px] "
+              src="https://i.ibb.co/PMmZZC0/FINAL-FULL-30.png"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+      <hr />
+      <section className="p-10">
+        <div className="grid md:flex justify-center items-center gap-5">
+          <img
+            className="w-[200px] rounded-2xl  md:w-[300px] "
+            src="https://i.ibb.co/whq8vdk/FINAL-FULL-26.png"
+            alt=""
+          />
+          <img
+            className="w-[200px] rounded-2xl    md:w-[300px] "
+            src="https://i.ibb.co/nBjpVgw/FINAL-FULL-27.png"
+            alt=""
+          />
+          <img
+            className="w-[200px] rounded-2xl  md:w-[350px] "
+            src="https://i.ibb.co/M9R9GMV/FINAL-FULL-28.png"
+            alt=""
+          />
+        </div>
+        <div className="space-y-2 grid  mt-5 p-10">
+          <h4 className="text-base font-medium ">
+            Distribution Board:
+            <div className="h-[2px] w-[140px]   bg-orange-500 "></div>
+          </h4>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 text-sm md:text-base ">
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>
+                  Low Voltage metal enclosed Distribution Board MDB, DB & SDB
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>All types of Motor Control panel (DOL, Star Delta etc.)</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>
+                  Stand Alone/Wall mounted or wall console type Distribution
+                  Board
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>In Accordance with Relevant IEC standard</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Comply with Accord & Alliance safety standard</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Compact & Customized design</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Dust And Vermin proof assembly</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Fast and simple Installation</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>IP Protection Panel</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <hr />
+
+      <section className="p-10">
+        <div className="grid md:flex justify-center items-center gap-5">
+          <img
+            className="w-[100px] rounded-2xl  md:w-[150px] "
+            src="https://i.ibb.co/PtPBG4G/FINAL-FULL-23.png"
+            alt=""
+          />
+          <img
+            className="w-[150px] rounded-2xl    md:w-[250px] "
+            src="https://i.ibb.co/ftV1ZYQ/FINAL-FULL-24.png"
+            alt=""
+          />
+          <img
+            className="w-[150px] rounded-2xl  md:w-[250px] "
+            src="https://i.ibb.co/xhRfmMn/FINAL-FULL-25.png"
+            alt=""
+          />
+        </div>
+        <div className="space-y-2 grid  mt-5 p-10">
+          <h4 className="text-base font-medium ">
+            On-line UPS:
+            <div className="h-[2px] w-[90px]   bg-orange-500 "></div>
+          </h4>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 text-sm md:text-base ">
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>On-line UPS double conversion</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Three-phase input/three-phase output</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>DSP control technology</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Power: from 10 to 200 KVA</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Galvanic insulation by output transformer</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Acoustic and luminous signalling</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Reduced harmonic distortion of output signal</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Highly efficient & Battery tested</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Output power factor 0.9</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>RS232/ SNMP-RJ45</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>
+                  GPRS module for mobile network remote monitoring (optional)
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Redundency Parallel (optional)</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>Touch screen LCD display & Manual bypass</p>
+              </div>
+            </div>
+            <div className="flex items-center gap-[4px]">
+              <div className="text-orange-500 ">
+                <TiSpanner />
+              </div>
+              <div>
+                <p>RFI filter</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ElectricalItem;
